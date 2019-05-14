b9b9c47b532b8a2d2ddee1fa54866a86ae2ae0d8
Vince Au <vinceau09@gmail.com>
2019-05-15 09:30:05 +1000

    Also export Merger

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let Merger = require('./lib/merger');
+const Merger = require('./lib/merger');
 
 const merge = async (file) => {
   const merger = new Merger({ delimeter: '\n\n' });
   return merger.processFile(file, true);
 }
 
-module.exports = merge;
+module.exports.Merger = Merger;
+module.exports.merge = merge;
