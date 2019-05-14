16378a4c7a43b5d59dfe51732efd31dd5deb1792
Vince Au <vinceau09@gmail.com>
2019-05-15 09:28:52 +1000

    Fix broken index.js from Github commit

--- index.js
+++ index.js
@@ -4,16 +4,5 @@ const merge = async (file) => {
   const merger = new Merger({ delimeter: '\n\n' });
   return merger.processFile(file, true);
 }
-    return new Promise(async (resolve, reject) => {
-        let merger = new Merger({ delimeter: '\n\n' });
-        let result;
-        try {
-            result = await merger.processFile(file, true);
-        } catch (e) {
-            reject(e);
-        }
-        resolve(result);
-    });
-};
 
 module.exports = merge;
